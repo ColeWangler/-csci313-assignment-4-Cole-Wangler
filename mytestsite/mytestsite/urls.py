ff7c8ae33b6a04f3e88d07d20a3faf7d412c03db
ColeWangler <cole.wangler@ndsu.edu>
2024-03-29 11:57:16 -0500

    added additional URL options to main project folder

--- mytestsite/mytestsite/urls.py
+++ mytestsite/mytestsite/urls.py
@@ -22,3 +22,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 ]
+
+# Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
